90c6b406ee9798460430944c36a3577465ed963b
Knokeros <knokeros@gmail.com>
2025-09-09 10:35:28 +0300

    Добавил логику и создал редми по заданию 2

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,55 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-@app.get("/tasks")
+
+class Task(BaseModel):
+    """Модель Задачи"""
+
+    id: int
+    title: str
+    status: str
+
+
+# Добаляю хранение в оперативной памяти
+tasks = []
+current_id = 1
+
+
+@app.get("/tasks", response_model=List[Task])
 def get_tasks():
-    pass
+    """ "Получение всвех задач"""
+    return tasks
 
-@app.post("/tasks")
-def create_task(task):
-    pass
 
-@app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+@app.post("/tasks", response_model=Task)
+def create_task(task_data: dict):
+    """Создание новой задачи"""
+    global current_id
+    task = Task(id=current_id, **task_data)
+    tasks.append(task)
+    current_id += 1
+    return task
+
+
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: int, task_data: dict):
+    """Обновление задачи"""
+    for task in tasks:
+        if task.id == task_id:
+            task.title = task_data.get("title", task.title)
+            task.status = task_data.get("status", task.status)
+            return task
+    raise HTTPException(status_code=404, detail="Задача не найдена")
+
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    global tasks
+    for i, task in enumerate(tasks):
+        if task.id == task_id:
+            tasks.pop(i)
+            return {"message": "Задача удалена"}
+    raise HTTPException(status_code=404, detail="Задача не найдена")
